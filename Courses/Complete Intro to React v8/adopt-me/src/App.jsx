9831cf856ecfebdd79fc39e0293a30619a31e023
MoYasser <41331651+MoYasser@users.noreply.github.com>
2024-08-06 19:42:55 +0300

    React Router Setup

--- Courses/Complete Intro to React v8/adopt-me/src/App.jsx
+++ Courses/Complete Intro to React v8/adopt-me/src/App.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Adopt Me!</h1>
+      </div>
+    </BrowserRouter>
   );
 };
 
