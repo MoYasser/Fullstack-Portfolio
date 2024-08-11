5f4c845fc37d6311ced5a67e86b1f40cc7b56f04
MoYasser <mohamed.yasser2000@ymail.com>
2024-08-11 14:38:21 +0300

    Starter PDA application Milestone

--- Courses/Udemy/Modern React Redux 2024/pdas/src/App.js
+++ Courses/Udemy/Modern React Redux 2024/pdas/src/App.js
@@ -1,13 +1,47 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
-import React, { Component } from "react";
-
+import AlexaImage from "./images/alexa.png";
+import SiriImage from "./images/siri.png";
+import CortanaImage from "./images/cortana.png";
 const App = () => {
   return (
     <div>
-      <div>Personal Digital Assitants</div>
-      <ProfileCard title={"Alexa"} handle={"@alexa99"} />
-      <ProfileCard title={"Cortana"} handle={"@cortana32"} />
-      <ProfileCard title={"Siri"} handle={"@siri01"} />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assitants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title={"Alexa"}
+                handle={"@alexa99"}
+                image={AlexaImage}
+                description="Aelxa was created by Amazon and helps you buy things"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title={"Cortana"}
+                handle={"@cortana32"}
+                image={CortanaImage}
+                description="Cortana was made by Microsoft. Who knows what it does?"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title={"Siri"}
+                handle={"@siri01"}
+                image={SiriImage}
+                description="siri was made by Apple and is being phased out"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
