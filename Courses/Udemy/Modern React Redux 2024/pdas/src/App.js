5eb0051d36eddb5761d7a188f4ee4c3eda4fbb9d
MoYasser <mohamed.yasser2000@ymail.com>
2024-08-11 12:25:01 +0300

    Added eslint and prettier

--- Courses/Udemy/Modern React Redux 2024/pdas/src/App.js
+++ Courses/Udemy/Modern React Redux 2024/pdas/src/App.js
@@ -1,14 +1,15 @@
 import ProfileCard from "./ProfileCard";
+import React, { Component } from "react";
 
 const App = () => {
-    return ( 
-        <div>
-            <div>Personal Digital Assitants</div>
-            <ProfileCard title={'Alexa'} handle={'@alexa99'}/>
-            <ProfileCard title={'Cortana'} handle={'@cortana32'}/>
-            <ProfileCard title={'Siri'} handle={'@siri01'}/>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <div>Personal Digital Assitants</div>
+      <ProfileCard title={"Alexa"} handle={"@alexa99"} />
+      <ProfileCard title={"Cortana"} handle={"@cortana32"} />
+      <ProfileCard title={"Siri"} handle={"@siri01"} />
+    </div>
+  );
+};
+
 export default App;
