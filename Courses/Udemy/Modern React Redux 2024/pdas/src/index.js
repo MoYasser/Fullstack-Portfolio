5d84120874751fc7b1dd5de7d57b13828e5f7c12
MoYasser <mohamed.yasser2000@ymail.com>
2024-08-11 12:19:49 +0300

    Added Pdas project

---
+++ Courses/Udemy/Modern React Redux 2024/pdas/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
+
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+root.render(<App/>)
