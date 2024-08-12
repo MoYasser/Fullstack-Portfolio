98f0bc74dcd8617b72a480d23f0ad52e73d1de0c
MoYasser <mohamed.yasser2000@ymail.com>
2024-08-12 14:05:48 +0300

    Section 4: EventHandlers, States Milestone

--- Courses/Udemy/Modern React Redux 2024/animals/src/App.js
+++ Courses/Udemy/Modern React Redux 2024/animals/src/App.js
@@ -1,10 +1,26 @@
+import React, { useState } from "react";
+import AnimalShow from "./AnimalShow";
+import "./App.css";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 const App = () => {
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    console.log("button clicked");
+    setAnimals([...animals, getRandomAnimal()]);
   };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add animal</button>
+      <div className="animal-list">{renderedAnimals}</div>
     </div>
   );
 };
